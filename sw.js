b2d097602beeddf6f8fa517b8095bdfcc29e4827
Vince <vsm2023-3243-73595@bicol-u.edu.ph>
2026-07-16 23:44:07 +0800

    v3 sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "wingsync-v2";
+const CACHE_NAME = "wingsync-v3";
 const urlsToCache = ["/index.html", "/app.js", "/style.css", "/manifest.json"];
 
 self.addEventListener("install", (event) => {
