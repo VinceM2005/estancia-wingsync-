2740dec907fc8a5bb6cc4f865974d360942d9141
Vince <vsm2023-3243-73595@bicol-u.edu.ph>
2026-07-22 23:46:15 +0800

    Added Login bg v21

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "wingsync-v20"; // Increment on every deployment
+const CACHE_NAME = "wingsync-v21"; // Increment on every deployment
 const urlsToCache = ["/index.html", "/app.js", "/style.css", "/manifest.json"];
 
 self.addEventListener("install", (event) => {
