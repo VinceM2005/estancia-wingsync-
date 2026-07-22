408cbabcb0698af8d724f1555cd6247a1b2bc0a2
Vince <vsm2023-3243-73595@bicol-u.edu.ph>
2026-07-22 19:56:54 +0800

    Scan QR code placement v18

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "wingsync-v17"; // Increment on every deployment
+const CACHE_NAME = "wingsync-v18"; // Increment on every deployment
 const urlsToCache = ["/index.html", "/app.js", "/style.css", "/manifest.json"];
 
 self.addEventListener("install", (event) => {
