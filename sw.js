261d5e3dd70574a4b47947bbed689ca7e0d8bd7b
Vince <vsm2023-3243-73595@bicol-u.edu.ph>
2026-07-25 13:57:10 +0800

    Added removed bg logo v26

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "wingsync-v25"; // Increment on every deployment
+const CACHE_NAME = "wingsync-v26"; // Increment on every deployment
 const urlsToCache = ["/index.html", "/app.js", "/style.css", "/manifest.json"];
 
 self.addEventListener("install", (event) => {
