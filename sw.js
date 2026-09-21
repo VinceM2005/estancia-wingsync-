6b78f3ef7896df66c18c3fa45ca52a90e9bb111b
Vince <vsm2023-3243-73595@bicol-u.edu.ph>
2026-09-21 13:41:46 +0800

    v178

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "wingsync-v177"; // Increment on every deployment
+const CACHE_NAME = "wingsync-v178"; // Increment on every deployment
 const urlsToCache = ["/index.html", "/app.js", "/style.css", "/manifest.json", "/wingsync-logo.png", "/logo.png", "/wingsync_cert-temp.png", "/tournament-cert-bg.jpg", "/mrpc-seal.png", "/season-rank-badges.jpg"];
 
 self.addEventListener("install", (event) => {
